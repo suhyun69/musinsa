34e36f648266d523d25dfb5cac0afc9bf0f46874
suhpark <suhpark@gmarket.com>
2023-11-20 21:28:27 +0900

    start.spring.io

---
+++ src/main/java/com/musinsa/suhpark/SuhparkApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.suhpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuhparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musinsa/suhpark/SuhparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.suhpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
