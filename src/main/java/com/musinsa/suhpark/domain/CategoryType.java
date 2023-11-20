97e34421db6f710c496eb30bfa25460e60066937
suhpark <suhpark@gmarket.com>
2023-11-21 00:29:00 +0900

    enum 수정

--- src/main/java/com/musinsa/suhpark/domain/CategoryType.java
+++ src/main/java/com/musinsa/suhpark/domain/CategoryType.java
@@ -4,14 +4,12 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum CategoryType {
-    TOP("T"),
-    OUTER("O"),
-    PANTS("P"),
-    SNEAKERS("S"),
-    BAG("B"),
-    HAT("H"),
-    SOCKS("C"),
-    ACCESSORIES("A");
-
-    private final String code;
+    TOP,
+    OUTER,
+    PANTS,
+    SNEAKERS,
+    BAG,
+    HAT,
+    SOCKS,
+    ACCESSORIES;
 }
