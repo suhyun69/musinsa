c2096bea50c3e474b736672aff7daf17e867af44
suhpark <suhpark@gmarket.com>
2023-11-25 09:51:04 +0900

    8개 카테고리 전체에 대한 상품 여부 및 임의의 카테고리에 대해 2개 이상의 상품 여부에 대한 고려

--- src/main/java/com/musinsa/suhpark/service/SearchService.java
+++ src/main/java/com/musinsa/suhpark/service/SearchService.java
@@ -39,12 +39,34 @@ public class SearchService {
 
     public LowestPriceBrand getLowestPriceBrand() {
 
-        Comparator<Brand> comparator = (b1, b2) -> Integer.compare(itemRepository.findByBrand(b1).stream().mapToInt(b -> b.getPrice()).sum(), itemRepository.findByBrand(b2).stream().mapToInt(b -> b.getPrice()).sum());
-        Brand lowestPriceBrand = brandRepository.findAll().stream()
+        // 8개 카테고리 상품을 모두 가지고 있는 브랜드 조회
+        List<Brand> fullyCategoryItemBrand = brandRepository.findAll().stream()
+                .filter(b -> itemRepository.findByBrand(b).stream().map(Item::getCategoryType).distinct().toList().size() == 8)
+                .collect(Collectors.toList());
+
+        Comparator<Brand> comparator = (b1, b2) -> Integer.compare(findItemByBrandWithLowestPrice(b1).stream().mapToInt(Item::getPrice).sum(), findItemByBrandWithLowestPrice(b2).stream().mapToInt(Item::getPrice).sum());
+        Brand lowestPriceBrand = fullyCategoryItemBrand.stream()
                 .min(comparator)
                 .get();
 
         return new LowestPriceBrand(itemRepository.findByBrand(lowestPriceBrand));
+    }
+
+    private List<Item> findItemByBrandWithLowestPrice(Brand brand) {
+
+        List<Item> itemList = itemRepository.findByBrand(brand);
+
+        // 임의의 카테고리에 2개 이상의 상품을 가진 브랜드인 경우, 최저가 상품을 계산에 포함
+        Arrays.stream(CategoryType.values())
+                .forEach(c -> {
+                    List<Item> itemListByCategory = itemList.stream()
+                            .filter(i -> i.getCategoryType() == c)
+                            .sorted(Comparator.comparing(Item::getPrice))
+                            .collect(Collectors.toList());
+                    itemList.add(itemListByCategory.get(0));
+                });
+
+        return itemList;
     }
 
     public BrandByCategory getHighestAndLowestBrandByCategory(CategoryType categoryType) {
